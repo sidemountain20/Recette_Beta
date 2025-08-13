e3232d06ca4809cca1621d956498ba0490223f20
Tetsuya Yokoyama <tetsuya.yokoyama@gmail.com>
2025-08-13 19:49:12 +0900

    Initial Commit

---
+++ .gitignore.swift
@@ -0,0 +1,7 @@
+//
+//  .gitignore.swift
+//  Recette
+//
+//  Created by 横山哲也 on 2025/08/13.
+//
+
---
+++ Recette/RecetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecetteApp.swift
+//  Recette
+//
+//  Created by 横山哲也 on 2025/08/11.
+//
+
+import SwiftUI
+
+@main
+struct RecetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
